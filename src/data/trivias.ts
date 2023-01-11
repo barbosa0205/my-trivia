b145ce1aee5c79fa73ce72e42e8e995d2a068a2c
jose luis barbosa cepeda <wichopianist@gmail.com>
2023-01-11 13:35:04 -0600

    creating new trivia category

--- src/data/trivias.ts
+++ src/data/trivias.ts
@@ -2,7 +2,7 @@ import { trivia } from '../interfaces/index'
 export const trivias: trivia[] = [
   {
     id: 'maths',
-    trivia: 'Matematicas',
+    trivia: 'Matématicas',
     image: '',
     desc: '',
     questions: [
@@ -23,6 +23,35 @@ export const trivias: trivia[] = [
           },
           {
             answer: '2',
+            correct: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'general_culture',
+    trivia: 'Cultura General',
+    image: '',
+    desc: '',
+    questions: [
+      {
+        question: '¿Quien pinto la monalisa?',
+        answers: [
+          {
+            answer: 'Frida Kahlo',
+            correct: false,
+          },
+          {
+            answer: 'Vincent Van Gogh',
+            correct: false,
+          },
+          {
+            answer: 'Pablo Picaso',
+            correct: false,
+          },
+          {
+            answer: 'Leonardo Davinci',
             correct: false,
           },
         ],
