0435761cd188ad8144da11949418138f700d3f14
jose luis barbosa cepeda <wichopianist@gmail.com>
2023-01-10 20:59:09 -0600

    create search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = () => {
   return (
     <div className='px-2'>
-      <i className='ri-search-eye-line text-2xl text-sky-400'></i>
+      <i className='ri-search-eye-line text-2xl text-sky-400 cursor-pointer'></i>
     </div>
   )
 }
