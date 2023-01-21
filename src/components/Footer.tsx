3290ae2a5c4fcb2cc5d5f0b736d946de74b41054
jose luis barbosa cepeda <wichopianist@gmail.com>
2023-01-21 15:06:11 -0600

    modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer className='w-full flex items-center justify-center bg-gray-100 h-20 text-gray-700 font-bold'>
+      My Trivia
+    </footer>
+  )
 }
 
 export default Footer
